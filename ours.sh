8676377330f057692eefa4bdf878bb213ce61931
4charwon <hu4charwon@hanyang.ac.kr>
2024-07-16 12:05:39 +0000

    ours.sh

---
+++ ours.sh
@@ -0,0 +1,8 @@
+
+python3 test.py --testset flickr --pth_name ours_sup.pth.tar
+python3 test.py --testset flickr --pth_name ours_selfsup.pth.tar
+python3 test.py --testset flickr --pth_name ours_sup_previs.pth.tar
+
+python3 test.py --testset vggss --pth_name ours_sup.pth.tar
+python3 test.py --testset vggss --pth_name ours_selfsup.pth.tar
+python3 test.py --testset vggss --pth_name ours_sup_previs.pth.tar
